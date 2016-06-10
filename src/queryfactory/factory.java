1aec8c09d42acd572799f9c119a691b6e4aa1b14
ru-orlov <ruorlov@gmail.com>
2016-06-10 21:56:03 +0300

    Update factory.java

--- src/queryfactory/factory.java
+++ src/queryfactory/factory.java
@@ -8,33 +8,32 @@ public class factory {
 
     public ResultSet getRows(){
         ArrayList<Object> params = new ArrayList<>();
-        String d_date = "2014-10-22";
+        String d_date = "0000-00-00";
         params.add(d_date);
-        String query = "SELECT * FROM mac_range WHERE Date = ? ";
+        String query = "SELECT * FROM table_name WHERE column_1 = ? ";
         return dbH.getRows(query, params);
     }
     
     public int updateRows(){
         String query;
         ArrayList<Object> params = new ArrayList<>();
-        String description_str = "new2 description";
-        int number_row = 36;
+        String description_str = "example description";
+        int number_row = 1;
         params.add(description_str);
         params.add(number_row);
-        query = "UPDATE mac_range SET Description = ? "
-              + "WHERE n = ?";
+        query = "UPDATE table_name SET column_1 = ? "
+              + "WHERE column_2 = ?";
         return dbH.doUpdate(query, params);
     }
     
     public int deleteRows(){  //or rows
         String query;
         ArrayList<Object> params = new ArrayList<>();
-        String description_str = "new2 description";
         int number_row = 36;
         params.add(description_str);
         params.add(number_row);
-            query = "DELETE FROM mac_range "
-                  + "WHERE n = ?";
+            query = "DELETE FROM table_name "
+                  + "WHERE column_2 = ?";
         return dbH.doUpdate(query, params);
     }
 }
